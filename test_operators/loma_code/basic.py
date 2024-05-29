aaef2111daa9aa75a43f1ecc1cce13916fdf89e3
CzzzzH <morphling233@gmail.com>
2024-05-28 18:54:51 -0700

    Add more basic operators

--- test_operators/loma_code/basic.py
+++ test_operators/loma_code/basic.py
@@ -1,11 +1,25 @@
 @simd
-def vector_add(x : In[Array[float]],
-               y : In[Array[float]],
-               z : Out[Array[float]]):
-    i : int = thread_id()
-    z[i] = x[i] + y[i]
+def add(x : In[Array[float]],
+        y : In[Array[float]],
+        z : Out[Array[float]],
+        out_features : In[int]):
+    batch_idx : int = thread_id() / out_features
+    feature_idx : int = thread_id() - batch_idx * out_features
+    z[batch_idx * out_features + feature_idx] = \
+        x[batch_idx * out_features + feature_idx] + \
+        y[batch_idx * out_features + feature_idx]
 
-grad_vector_add = rev_diff(vector_add)
+grad_add = rev_diff(add)
+
+@simd
+def sum(input: In[Array[float]],
+        output: Out[Array[float]],
+        in_features: In[int]):
+    batch_idx : int = thread_id() / in_features
+    feature_idx : int = thread_id() - batch_idx * in_features
+    atomic_add(output[batch_idx], input[batch_idx * in_features + feature_idx])
+
+grad_sum = rev_diff(sum)
 
 @simd
 def linear(input: In[Array[float]],
@@ -23,8 +37,21 @@ def linear(input: In[Array[float]],
         i = i + 1
     tmp = tmp + bias[feature_idx]
     output[batch_idx * out_features + feature_idx] = tmp
-    
+
 grad_linear = rev_diff(linear)
+
+@simd
+def relu(input: In[Array[float]],
+         output: Out[Array[float]],
+         out_features: In[int]):
+    batch_idx : int = thread_id() / out_features
+    feature_idx : int = thread_id() - batch_idx * out_features
+    if input[batch_idx * out_features + feature_idx] < 0:
+        output[batch_idx * out_features + feature_idx] = 0
+    else:
+        output[batch_idx * out_features + feature_idx] = input[batch_idx * out_features + feature_idx]
+
+grad_relu = rev_diff(relu)
 
 @simd
 def silu(input: In[Array[float]],
@@ -36,3 +63,4 @@ def silu(input: In[Array[float]],
         1.0 / (1.0 + exp(-input[batch_idx * out_features + feature_idx]))
 
 grad_silu = rev_diff(silu)
+
--- test_operators/test_basic.py
+++ test_operators/test_basic.py
@@ -16,47 +16,75 @@ def test_basic_ispc():
                                   target = 'ispc',
                                   output_filename = '_code/basic')
     
-    # Test vector_add
-    v1 = torch.rand(10, requires_grad=True)
-    v2 = torch.rand(10, requires_grad=True)
-    v1_ctype = build_ctypes(v1, 10)
-    v1_grad_ctype = build_ctypes(torch.zeros_like(v1), 10)
-    v2_ctype = build_ctypes(v2, 10)
-    v2_grad_ctype = build_ctypes(torch.zeros_like(v2), 10)
-    v3_ctype = build_ctypes(torch.zeros(10), 10)
-    lib.vector_add(v1_ctype, v2_ctype, v3_ctype, 10)
-    v3_out = build_tensor(v3_ctype, (10,))
+    # Test add
+    batch_size = 4
+    in_features = out_features = 10
+    in_shape = (batch_size, in_features)
+    out_shape = (batch_size, out_features)
+    num_threads = batch_size * out_features
+    v1 = torch.rand(in_shape, requires_grad=True)
+    v2 = torch.rand(in_shape, requires_grad=True)
+    v1_ctype = build_ctypes(v1, batch_size * in_features)
+    v1_grad_ctype = build_ctypes(torch.zeros_like(v1), batch_size * in_features)
+    v2_ctype = build_ctypes(v2, batch_size * in_features)
+    v2_grad_ctype = build_ctypes(torch.zeros_like(v2), batch_size * in_features)
+    v3_ctype = build_ctypes(torch.zeros(out_shape), out_features * batch_size)
+    lib.add(v1_ctype, v2_ctype, v3_ctype, in_features, num_threads)
+    v3_out = build_tensor(v3_ctype, out_shape)
     v3_ref = v1 + v2
-    check_res(v3_out, v3_ref, "vector_add")
+    check_res(v3_out, v3_ref, "add")
 
-    # Test grad_vector_add
+    # Test grad_add
     v3_ref.backward(v3_ref)
-    lib.grad_vector_add(v1_ctype, v1_grad_ctype, v2_ctype, v2_grad_ctype, v3_ctype, 10)
-    v1_grad = build_tensor(v1_grad_ctype, (10,))
-    v2_grad = build_tensor(v2_grad_ctype, (10,))
-    check_res(v1_grad, v1.grad, "grad_vector_add_1")
-    check_res(v2_grad, v2.grad, "grad_vector_add_2")
+    lib.grad_add(v1_ctype, v1_grad_ctype, v2_ctype, v2_grad_ctype, v3_ctype, in_features, ctypes.c_int(0), num_threads)
+    v1_grad = build_tensor(v1_grad_ctype, in_shape)
+    v2_grad = build_tensor(v2_grad_ctype, in_shape)
+    check_res(v1_grad, v1.grad, "grad_add_1")
+    check_res(v2_grad, v2.grad, "grad_add_2")
+
+    # Test sum
+    batch_size = 4
+    in_features = 10
+    out_features = 1
+    in_shape = (batch_size, in_features)
+    out_shape = (batch_size,)
+    num_threads = batch_size * in_features
+    input = torch.rand(in_shape, requires_grad=True)
+    input_ctype = build_ctypes(input, batch_size * in_features)
+    input_grad_ctype = build_ctypes(torch.zeros_like(input), batch_size * in_features)
+    output_ctype = build_ctypes(torch.zeros(out_shape), batch_size * out_features)
+    lib.sum(input_ctype, output_ctype, in_features, num_threads)
+    output_out = build_tensor(output_ctype, out_shape)
+    output_ref = torch.sum(input, dim=1)
+    check_res(output_out, output_ref, "sum")
+
+    # Test grad_sum
+    output_ref.backward(output_ref)
+    lib.grad_sum(input_ctype, input_grad_ctype, output_ctype, in_features, ctypes.c_int(0), num_threads)
+    input_grad = build_tensor(input_grad_ctype, in_shape)
+    check_res(input_grad, input.grad, "grad_sum")
 
     # Test linear
     batch_size = 4
     in_features = 10
     out_features = 5
-    input = torch.rand(batch_size, in_features, requires_grad=True)
-    weight = torch.rand(out_features, in_features, requires_grad=True)
-    bias = torch.rand(out_features, requires_grad=True)
-    output = torch.zeros(batch_size, out_features)
+    in_shape = (batch_size, in_features)
+    out_shape = (batch_size, out_features)
+    num_threads = batch_size * out_features
+    input = torch.rand(in_shape, requires_grad=True)
+    weight = torch.rand((out_features, in_features), requires_grad=True)
+    bias = torch.rand((out_features,), requires_grad=True)
     torch_linear = torch.nn.Linear(in_features, out_features)
     torch_linear.weight.data = weight
     torch_linear.bias.data = bias
     
     input_ctype = build_ctypes(input, in_features * batch_size)
-    input_grad_ctype = build_ctypes(torch.zeros_like(input), in_features * batch_size)
+    input_grad_ctype = build_ctypes(torch.zeros_like(input), batch_size * in_features)
     weight_ctype = build_ctypes(weight, in_features * out_features)
-    weight_grad_ctype = build_ctypes(torch.zeros_like(weight), in_features * out_features)  
+    weight_grad_ctype = build_ctypes(torch.zeros_like(weight), out_features * in_features)
     bias_ctype = build_ctypes(bias, out_features)
     bias_grad_ctype = build_ctypes(torch.zeros_like(bias), out_features)
-    output_ctype = build_ctypes(output, out_features * batch_size)
-    num_threads = batch_size * out_features
+    output_ctype = build_ctypes(torch.zeros(out_shape), batch_size * out_features)
     lib.linear(input_ctype, weight_ctype, bias_ctype, output_ctype, in_features, out_features, num_threads)
     output_out = build_tensor(output_ctype, (batch_size, out_features))
     output_ref = torch_linear(input)
@@ -66,35 +94,60 @@ def test_basic_ispc():
     output_ref.backward(output_ref)
     lib.grad_linear(input_ctype, input_grad_ctype, weight_ctype, weight_grad_ctype, bias_ctype, bias_grad_ctype,
                     output_ctype, in_features, ctypes.c_int(0), out_features, ctypes.c_int(0), num_threads)
-    input_grad = build_tensor(input_grad_ctype, (batch_size, in_features))
+    input_grad = build_tensor(input_grad_ctype, in_shape)
     weight_grad = build_tensor(weight_grad_ctype, (out_features, in_features))
     bias_grad = build_tensor(bias_grad_ctype, (out_features,))
     check_res(input_grad, input.grad, "grad_linear_input")
     check_res(weight_grad, torch_linear.weight.grad, "grad_linear_weight")
     check_res(bias_grad, torch_linear.bias.grad, "grad_linear_bias")
 
+    # Test relu
+    batch_size = 4
+    in_features = out_features = 10
+    in_shape = (batch_size, in_features)
+    out_shape = (batch_size, out_features)
+    num_threads = batch_size * out_features
+    input = torch.rand(in_shape, requires_grad=True)
+    output = torch.zeros(out_shape)
+    torch_relu = torch.nn.ReLU()
+    input_ctype = build_ctypes(input, batch_size * in_features)
+    input_grad_ctype = build_ctypes(torch.zeros_like(input), batch_size * in_features)
+    output_ctype = build_ctypes(output, batch_size * out_features)
+    lib.relu(input_ctype, output_ctype, out_features, num_threads)
+    output_out = build_tensor(output_ctype, out_shape)
+    output_ref = torch_relu(input)
+    check_res(output_out, output_ref, "relu")
+
+    # Test grad_relu
+    output_ref.backward(output_ref)
+    lib.grad_relu(input_ctype, input_grad_ctype, output_ctype, out_features, ctypes.c_int(0), num_threads)
+    input_grad = build_tensor(input_grad_ctype, out_shape)
+    check_res(input_grad, input.grad, "grad_relu")
+
     # Test silu
     batch_size = 4
-    out_features = 20
-    input = torch.rand(batch_size, out_features, requires_grad=True)
-    output = torch.zeros(batch_size, out_features)
-    torch_silu = torch.nn.SiLU()
-    input_ctype = build_ctypes(input, out_features * batch_size)
-    input_grad_ctype = build_ctypes(torch.zeros_like(input), out_features * batch_size)
-    output_ctype = build_ctypes(output, out_features * batch_size)
+    in_features = out_features = 10
+    in_shape = (batch_size, in_features)
+    out_shape = (batch_size, out_features)
     num_threads = batch_size * out_features
+    input = torch.rand(in_shape, requires_grad=True)
+    output = torch.zeros(out_shape)
+    torch_silu = torch.nn.SiLU()
+    input_ctype = build_ctypes(input, batch_size * in_features)
+    input_grad_ctype = build_ctypes(torch.zeros_like(input), batch_size * in_features)
+    output_ctype = build_ctypes(output, batch_size * out_features)
     lib.silu(input_ctype, output_ctype, out_features, num_threads)
-    output_out = build_tensor(output_ctype, (batch_size, out_features))
+    output_out = build_tensor(output_ctype, out_shape)
     output_ref = torch_silu(input)
     check_res(output_out, output_ref, "silu")
 
     # Test grad_silu
     output_ref.backward(output_ref)
     lib.grad_silu(input_ctype, input_grad_ctype, output_ctype, out_features, ctypes.c_int(0), num_threads)
-    input_grad = build_tensor(input_grad_ctype, (batch_size, out_features))
+    input_grad = build_tensor(input_grad_ctype, out_shape)
     check_res(input_grad, input.grad, "grad_silu")
 
 if __name__ == '__main__':
-    
+
     test_basic_ispc()
     
