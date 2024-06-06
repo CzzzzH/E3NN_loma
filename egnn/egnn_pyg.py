a8d58d66f022323f5fef5e6721d9b178381b14ce
fishming <1661342068@qq.com>
2024-06-05 17:09:05 -0700

    [Update] egnn pyg

--- egnn/egnn_pyg.py
+++ egnn/egnn_pyg.py
@@ -8,46 +8,85 @@ from torch.nn import GRU, Linear, ReLU, Sequential
 import torch_geometric.transforms as T
 from torch_geometric.datasets import QM9
 from torch_geometric.loader import DataLoader
-from torch_geometric.nn import NNConv, Set2Set
+from torch_geometric.nn import NNConv, Set2Set, aggr
 from torch_geometric.utils import remove_self_loops
 from dataloader.qm9 import dataQM9
 
-target = 0
-dim = 64
 
 
-dataset = dataQM9(target=target)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import Set2Set
 
-class Net(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lin0 = torch.nn.Linear(dataset.num_features, dim)
+class EGNNLayer(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(EGNNLayer, self).__init__()
+        self.edge_mlp = nn.Sequential(
+            # edge_attr is 5-dimensional
+            nn.Linear(2 * in_features + 5, out_features),
+            nn.ReLU(),
+            nn.Linear(out_features, out_features)
+        )
+        self.node_mlp = nn.Sequential(
+            nn.Linear(in_features + out_features, out_features),
+            nn.ReLU(),
+            nn.Linear(out_features, out_features)
+        )
+        self.coord_mlp = nn.Sequential(
+            nn.Linear(out_features, 1),
+            nn.ReLU(),
+            nn.Linear(1, 1)
+        )
 
-        nn = Sequential(Linear(5, 128), ReLU(), Linear(128, dim * dim))
-        self.conv = NNConv(dim, dim, nn, aggr='mean')
-        self.gru = GRU(dim, dim)
+    def forward(self, x, edge_index, edge_attr, pos):
+        row, col = edge_index
+        edge_input = torch.cat([x[row], x[col], edge_attr], dim=1)
+        edge_features = self.edge_mlp(edge_input)
+        
+        aggregated_edge_features = torch.zeros_like(x)
+        aggregated_edge_features.index_add_(0, row, edge_features)
+        
+        node_input = torch.cat([x, aggregated_edge_features], dim=1)
+        node_features = self.node_mlp(node_input)
+        
+        relative_pos = pos[row] - pos[col]
+        coord_updates = self.coord_mlp(edge_features) * relative_pos
+        coord_updates = coord_updates.sum(dim=0, keepdim=True)
+        
+        pos = pos + coord_updates
+        
+        return node_features, pos
 
+class Net(nn.Module):
+    def __init__(self, dataset, dim):
+        super(Net, self).__init__()
+        self.lin0 = nn.Linear(dataset.num_features, dim)
+        self.egnn1 = EGNNLayer(dim, dim)
+        self.egnn2 = EGNNLayer(dim, dim)
+        self.egnn3 = EGNNLayer(dim, dim)
         self.set2set = Set2Set(dim, processing_steps=3)
-        self.lin1 = torch.nn.Linear(2 * dim, dim)
-        self.lin2 = torch.nn.Linear(dim, 1)
+        self.lin1 = nn.Linear(2 * dim, dim)
+        self.lin2 = nn.Linear(dim, 1)
 
     def forward(self, data):
-        out = F.relu(self.lin0(data.x))
-        h = out.unsqueeze(0)
+        x = F.relu(self.lin0(data.x))
+        pos = data.pos
 
-        for i in range(3):
-            m = F.relu(self.conv(out, data.edge_index, data.edge_attr))
-            out, h = self.gru(m.unsqueeze(0), h)
-            out = out.squeeze(0)
+        x, pos = self.egnn1(x, data.edge_index, data.edge_attr, pos)
+        x, pos = self.egnn2(x, data.edge_index, data.edge_attr, pos)
+        x, pos = self.egnn3(x, data.edge_index, data.edge_attr, pos)
 
-        out = self.set2set(out, data.batch)
-        out = F.relu(self.lin1(out))
-        out = self.lin2(out)
-        return out.view(-1)
+        x = self.set2set(x, data.batch)
+        x = F.relu(self.lin1(x))
+        x = self.lin2(x)
+        return x.view(-1)
 
-
+target = 0
+dim = 64
+dataset = dataQM9(target=target)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net().to(device)
+model = Net(dataset, dim).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                        factor=0.7, patience=5,
